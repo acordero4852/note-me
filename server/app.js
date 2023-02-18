4dfb9407bf313c63803fefd1ddda3340fe8b73cd
ri-tae-ka <ritzthareja@gmail.com>
2023-02-18 13:22:28 +0530

    backend error handling

--- server/app.js
+++ server/app.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const errorMiddleware = require("./middleware/error");
+
 app.use(express.json());
 
 //routes
 const notes = require("./routes/notesRoute");
 
 app.use("/api/v1", notes);
+
+//middleware for errors
+app.use(errorMiddleware);
 
 module.exports = app;
